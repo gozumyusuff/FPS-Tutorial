c29e2cbc66199d7f244b0f2685447ad08bf8b251
gozumyusuff <139639043+gozumyusuff@users.noreply.github.com>
2023-10-20 17:23:26 +0300

    initial

---
+++ Assets/Bananadev's Pack/MouseLook.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float sensitivity = 2.0f; // Fare hassasiyeti
+    public float smoothing = 2.0f; // Fare düzeltme
+
+    private Vector2 mouseLook;
+    private Vector2 smoothV;
+
+    private GameObject player;
+
+    void Start()
+    {
+        player = this.transform.parent.gameObject;
+    }
+
+    void Update()
+    {
+        // Fare girişini al
+        Vector2 mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        // Fare girişini yumuşatma
+        mouseDelta = Vector2.Scale(mouseDelta, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        smoothV.x = Mathf.Lerp(smoothV.x, mouseDelta.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, mouseDelta.y, 1f / smoothing);
+        mouseLook += smoothV;
+
+        // Minimum ve maksimum açı sınırlamaları eklemek isterseniz burada yapabilirsiniz
+
+        // Yatay (sol-sağ) dönme
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+
+        // Dikey (yukarı-aşağı) dönme
+        player.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, player.transform.up);
+    }
+}
---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+
+    public GameObject player;
+    [Space]
+    public Transform spawnPoint;
+
+    void Start()
+    {
+        Debug.Log("Connecting...");
+
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+
+        Debug.Log("Connected to Server");
+
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        
+        Debug.Log("we are in the lobby");
+
+        PhotonNetwork.JoinOrCreateRoom("test", null, null);
+        
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+
+        Debug.Log("we are connected and in a room");
+
+        GameObject _player = PhotonNetwork.Instantiate(player.name, spawnPoint.position, Quaternion.identity);
+        
+        _player.GetComponent<PlayerSetup>().IsLocalPlayer();
+    }
+
+}
+
